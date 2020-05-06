4064b4ad0d2e4db960f51fb52d3fe8632ea83aaf
Enes Dogan <e.dogan@hymnai.com>
2020-05-06 19:41:42 +0100

    init

---
+++ src/main/java/itp/bootcamp/ecommercedemo/EcommercedemoApplication.java
@@ -0,0 +1,13 @@
+package itp.bootcamp.ecommercedemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EcommercedemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EcommercedemoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/itp/bootcamp/ecommercedemo/EcommercedemoApplicationTests.java
@@ -0,0 +1,13 @@
+package itp.bootcamp.ecommercedemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EcommercedemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
